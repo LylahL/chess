734be919547c612ae8812b4b0a812083df26e76f
LylahL <leimin304@gmail.com>
2024-02-26 20:10:24 -0700

    DataAccessInterface

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,19 @@
 package server;
 
-import handler.ClearHandler;
 import spark.Spark;
-
-import java.nio.file.Paths;
 
 /*The Server receives network HTTP requests and sends them to the correct handler for processing.
   The server should also handle all unhandled exceptions that your application generates and return the appropriate HTTP status code.
 */
 
 public class Server {
-
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
-        var webDir = Paths.get(Server.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "web");
-        Spark.externalStaticFileLocation(webDir.toString());
+        Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        Spark.delete("/db",(request, response) -> new ClearHandler().clearDataBase(request, response));
+
 
         Spark.awaitInitialization();
         return Spark.port();

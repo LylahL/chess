f2b6de00dd3ac71878cff10601f8602f27bb0a47
LylahL <leimin304@gmail.com>
2024-02-28 10:56:02 -0700

    unit test set up

---
+++ server/src/test/java/serviceTests/service.java
@@ -0,0 +1,39 @@
+package serviceTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class service {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void clearDataBase() {
+  }
+
+  @Test
+  void register() {
+  }
+
+  @Test
+  void login() {
+  }
+
+  @Test
+  void logout() {
+  }
+
+  @Test
+  void listGames() {
+  }
+
+  @Test
+  void createGame() {
+  }
+
+  @Test
+  void joinGame() {
+  }
+}
